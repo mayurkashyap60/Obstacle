eb9aecba4493188acce93d9546c2c1e3884f54fd
mayurkashyap <mayurkashyap60@gmail.com>
2020-02-05 11:02:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/obstacle/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.obstacle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_result);
+        Button btn = findViewById(R.id.btn);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse() {
+
+        String url = "https://api.myjson.com/bins/1frxl2";
+        JsonObjectRequest mObject = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    JSONArray mArray = response.getJSONArray("employees");
+
+                    for (int i = 0; i < mArray.length(); i++){
+                        JSONObject object = mArray.getJSONObject(i);
+
+                        String fName = object.getString("name");
+                        String fAddress = object.getString("address");
+
+                        textView.append(fName + ", " + fAddress + "\n\n");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(mObject);
+    }
+}
